231a099bb3c4eec90e30ab5e0ec007f1c3025499
Pauline C <poc215@nyu.edu>
2018-09-18 11:26:22 -0400

    changed server binding

--- app.js
+++ app.js
@@ -388,17 +388,16 @@ app.get('/addAllTestVenues', (req,res)=>{
 
 
  // Initialize the app
- 
+
  // process.env.PORT lets the port be set by Heroku
   var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
   });
-});
 
 
 
 
-app.listen(8080);
+//app.listen(8080);
 
 
